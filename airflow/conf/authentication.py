7ad106623b202bd74c80eea2df87d5ca4a912ae2
lidong <lidong@k2data.com.cn>
2016-12-14 09:48:55 +0800

    add authenticate

---
+++ airflow/conf/authentication.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import airflow
+from airflow import models, settings
+from airflow.contrib.auth.backends.password_auth import PasswordUser
+
+user = PasswordUser(models.User())
+user.username = 'k2data'
+user.email = 'lidong@k2data.com.cn'
+user.password = 'passw0rd'
+session = settings.Session()
+session.add(user)
+session.commit()
+session.close()
