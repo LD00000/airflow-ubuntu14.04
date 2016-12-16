35109c7c0e58346624b8ccb2c3ac3a20bd8b08e9
LD00000 <lidong9144@163.com>
2016-12-17 03:57:22 +0800

    Fix bug

--- airflow/script/entrypoint.sh
+++ airflow/script/entrypoint.sh
@@ -23,11 +23,6 @@ if [ -e "/requirements.txt" ]; then
     $(which pip) install --user -r /requirements.txt
 fi
 
-if [ "x$AUTH" = "xy" ]; then
-    echo "authentication..."
-    exec python ${AIRFLOW_HOME}/authentication.py
-fi
-
 # wait for DB
 if [ "$1" = "webserver" ] || [ "$1" = "worker" ] || [ "$1" = "scheduler" ] ; then
     i=0
@@ -63,11 +58,11 @@ then
       sleep 5
     done
     fi
-    exec $CMD "$@"
+    exec $CMD "$@" | python ${AIRFLOW_HOME}/authentication.py
 elif [ "x$EXECUTOR" = "xLocal" ]
 then
     sed -i "s/executor = CeleryExecutor/executor = LocalExecutor/" "$AIRFLOW_HOME"/airflow.cfg
-    exec $CMD "$@"
+    exec $CMD "$@" | python ${AIRFLOW_HOME}/authentication.py
 else
     if [ "$1" = "version" ]; then
         exec $CMD version
@@ -76,5 +71,5 @@ else
     sed -i "s#sql_alchemy_conn = mysql://root:root@192.168.130.64:3306/airflow#sql_alchemy_conn = sqlite:////usr/local/airflow/airflow.db#" "$AIRFLOW_HOME"/airflow.cfg
     echo "Initialize database..."
     $CMD initdb
-    exec $CMD webserver
+    exec $CMD webserver | python ${AIRFLOW_HOME}/authentication.py
 fi
